e32991c5ea3426315518541220d40f250fe33d4e
Daniel Fernandes <danielarruda99@gmail.com>
2026-03-15 20:40:40 -0300

    criando tela de vagas

--- src/main/java/br/com/danielarruda/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/danielarruda/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -24,6 +24,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -81,5 +83,10 @@ public class CandidateController {
 
     }
     
+    @GetMapping("/jobs")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String jobs() {
+        return "candidate/jobs";
+    }
     
 }
